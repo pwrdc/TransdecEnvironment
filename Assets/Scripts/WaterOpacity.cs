7dbc475a1620f8bf3d42a31b39032dd78eb8faa0
PiotrJZielinski <piotrekzie100@gmail.com>
2018-11-20 18:22:47 +0100

    added water_opacity script and fitted it in the env; added agent cameras

---
+++ Assets/Scripts/WaterOpacity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterOpacity : MonoBehaviour {
+
+    public GameObject water_surface;
+    private bool underwater;
+    public Color water_color = new Color(0.22f, 0.65f, 0.77f, 0.5f);
+    public float water_fog = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+        RenderSettings.fogMode = FogMode.Exponential;
+        RenderSettings.fogColor = water_color;
+        RenderSettings.fogDensity = water_fog;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if ((transform.position.y < water_surface.transform.position.y) != underwater)
+        {
+            underwater = (transform.position.y < water_surface.transform.position.y);
+            if (underwater) SetUnderwater();
+            else SetNormal();
+        }
+	}
+
+    void SetNormal()
+    {
+        RenderSettings.fog = false;
+        Debug.Log("Normal");
+    }
+
+    void SetUnderwater()
+    {
+        RenderSettings.fog = true;
+        Debug.Log("Underwater");
+    }
+}
+ 
