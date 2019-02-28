e6ae23492fa695e71e8794ef4181242bdcb83636
PiotrJZielinski <piotrekzie100@gmail.com>
2019-02-28 14:15:01 +0100

    Water opacity bugfix

--- Assets/Scripts/WaterOpacity.cs
+++ Assets/Scripts/WaterOpacity.cs
@@ -8,27 +8,21 @@ public class WaterOpacity : MonoBehaviour {
     private bool underwater;
     public Color waterColor = new Color(0.22f, 0.65f, 0.77f, 0.5f);
     public float waterFog = 0.2f;
-    public bool dataCollecting = false;
 
-	// Use this for initialization
 	void Start () {
         RenderSettings.fogMode = FogMode.Exponential;
-        RenderSettings.fogColor = waterColor;
-        RenderSettings.fogDensity = waterFog;
     }
 	
-	// Update is called once per frame
 	void Update () {
-        if (((transform.position.y < waterSurface.transform.position.y) != underwater) && (!dataCollecting))
-        {
+        if (waterColor != RenderSettings.fogColor)
+            RenderSettings.fogColor = waterColor;
+        if (waterFog != RenderSettings.fogDensity)
+            RenderSettings.fogDensity = waterFog;
+        if ((transform.position.y < waterSurface.transform.position.y) != underwater) {
             underwater = (transform.position.y < waterSurface.transform.position.y);
             if (underwater) SetUnderwater();
             else SetNormal();
         }
-        if (waterFog != RenderSettings.fogDensity)
-            RenderSettings.fogDensity = waterFog;
-        if (waterColor != RenderSettings.fogColor)
-            RenderSettings.fogColor = waterColor;
 	}
 
     public void SetNormal()
