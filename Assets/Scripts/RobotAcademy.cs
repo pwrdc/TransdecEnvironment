8ce92d032b1230071d78006928f4499d099be3d5
PiotrJZielinski <piotrekzie100@gmail.com>
2019-02-28 14:15:38 +0100

    Setting for initialization with Learning or Player brain

--- Assets/Scripts/RobotAcademy.cs
+++ Assets/Scripts/RobotAcademy.cs
@@ -1,3 +1,31 @@
 ﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class RobotAcademy : Academy { }
+public enum Control {
+    learning, player
+}
+
+public class RobotAcademy : Academy { 
+
+    [Header("Controller settings")]
+    public Control control;
+    public Brain learningBrain;
+    public Brain playerBrain;
+
+    RobotAgent robot;
+
+    public override void InitializeAcademy() {
+        robot = GameObject.Find("Robot").GetComponent<RobotAgent>();
+        if (control == Control.learning) {
+            robot.GiveBrain(learningBrain);
+            broadcastHub.broadcastingBrains.Add(learningBrain);
+            broadcastHub.SetControlled(learningBrain, true);
+        }
+        else {
+            robot.GiveBrain(playerBrain);
+            broadcastHub.broadcastingBrains.Add(playerBrain);
+        }
+    }
+}
