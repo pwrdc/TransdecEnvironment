0dfeead2b7a7ae8035b6d9b43ced6565d0a83e8c
Patryk Gańczarek <patrykganczarek@MacBook-Pro-Patryk.local>
2019-12-09 18:57:37 +0100

    Add GUI to simulation

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Menu"); //Tutaj nazwa sceny do pobrania z ustawien
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
