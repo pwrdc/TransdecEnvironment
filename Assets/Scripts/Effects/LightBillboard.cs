f58c1c9a07858818bbfb450106acbfcf82d3f896
piotrszleg <szlegp@gmail.com>
2020-05-29 02:20:20 +0200

    The best yet possible fake light ray implementation. Uses a quad rotating around its local Y axis towards camera with softened triangle texture and soft particles material.

---
+++ Assets/Scripts/Effects/LightBillboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Basically we want this thing to rotate 
+ * around it's local Y axis towards currently rendering camera.
+ * This could be a shader, but it'd require implementing 
+ * LookAt in shader language.
+ */
+public class LightBillboard : MonoBehaviour
+{
+    Vector3 up;
+
+    void Start()
+    {
+        up = transform.up;
+    }
+
+    void OnRenderObject()
+    {
+        Vector3 observer = Camera.current.transform.position;
+        Vector3 onPlane = new Vector3(observer.x, transform.position.y, observer.z);
+        transform.LookAt(onPlane, up);
+    }
+}
