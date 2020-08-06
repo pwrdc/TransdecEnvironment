14177492a0d2ce526809c5df7798ea1b01d35e72
piotrszleg <szlegp@gmail.com>
2020-08-06 06:20:12 +0200

    Renamed functionality folder and removed namespace.

--- Assets/Scripts/Robot/functionality/BallGrapper.cs
+++ Assets/Scripts/Robot/functionality/BallGrapper.cs
@@ -1,90 +1,87 @@
 ﻿using UnityEngine;
 
-namespace Robot.Functionality
+public class BallGrapper : MonoBehaviour
 {
-    public class BallGrapper : MonoBehaviour
+    public LayerMask mask=0;
+    public Vector3 offSet=Vector3.zero;
+    public float range = 0.5f;
+    public enum State
     {
-        public LayerMask mask=0;
-        public Vector3 offSet=Vector3.zero;
-        public float range = 0.5f;
-        public enum State
-        {
-            Holding, ObjectInReach, NoObjectsInReach
-        }
-        State state= State.NoObjectsInReach;
-        Transform objectInReach;
-        public float holdingTimeot = 0.5f;
-        bool grabHeld = false;
+        Holding, ObjectInReach, NoObjectsInReach
+    }
+    State state= State.NoObjectsInReach;
+    Transform objectInReach;
+    public float holdingTimeot = 0.5f;
+    bool grabHeld = false;
 
-        public void Grab()
+    public void Grab()
+    {
+        grabHeld = true;
+        UpdateReachingState();
+        if (state == State.NoObjectsInReach)
         {
-            grabHeld = true;
-            UpdateReachingState();
-            if (state == State.NoObjectsInReach)
-            {
-                EventsLogger.Log("Attempted to grab something.");
-            }
-            if (state == State.ObjectInReach)
-            {
-                state = State.Holding;
-                EventsLogger.Log("Started holding an object.");
-            }
+            EventsLogger.Log("Attempted to grab something.");
         }
-
-        public State GetState() 
+        if (state == State.ObjectInReach)
         {
-            UpdateReachingState();
-            return state;
+            state = State.Holding;
+            EventsLogger.Log("Started holding an object.");
         }
+    }
 
-        private void Update()
-        {
-            if (state == State.Holding)
-            {
-                if (grabHeld)
-                    objectInReach.position = Vector3.Lerp(objectInReach.position, transform.position + transform.TransformDirection(offSet), 0.5f);
-                else
-                {
-                    state = State.NoObjectsInReach;
-                    EventsLogger.Log("Stopped holding an object.");
-                }
-                grabHeld = false;
-            }
-        }
+    public State GetState() 
+    {
+        UpdateReachingState();
+        return state;
+    }
 
-        // Finds closest object in range
-        Transform FindObjectToHold()
+    private void Update()
+    {
+        if (state == State.Holding)
         {
-            Collider[] colliders = Physics.OverlapSphere(transform.position, range, mask);
-            float closestDistance = range;
-            Collider closest = null;
-            foreach (var collider in colliders)
-            {
-                float distance = Vector3.Distance(collider.transform.position, transform.position);
-                if (distance < closestDistance)
-                {
-                    closest = collider;
-                    closestDistance = distance;
-                }
-            }
-            return closest?.transform;
-        }
-
-        private void UpdateReachingState() 
-        {
-            if (state == State.Holding)
-                return;
-            objectInReach = FindObjectToHold();
-            if(objectInReach!=null)
-                state = State.ObjectInReach;
+            if (grabHeld)
+                objectInReach.position = Vector3.Lerp(objectInReach.position, transform.position + transform.TransformDirection(offSet), 0.5f);
             else
+            {
                 state = State.NoObjectsInReach;
+                EventsLogger.Log("Stopped holding an object.");
+            }
+            grabHeld = false;
         }
+    }
 
-        private void OnDrawGizmosSelected()
+    // Finds closest object in range
+    Transform FindObjectToHold()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, range, mask);
+        float closestDistance = range;
+        Collider closest = null;
+        foreach (var collider in colliders)
         {
-            Gizmos.color = Color.gray;
-            Gizmos.DrawWireSphere(transform.position, range);
+            float distance = Vector3.Distance(collider.transform.position, transform.position);
+            if (distance < closestDistance)
+            {
+                closest = collider;
+                closestDistance = distance;
+            }
         }
+        return closest?.transform;
+    }
+
+    private void UpdateReachingState() 
+    {
+        if (state == State.Holding)
+            return;
+        objectInReach = FindObjectToHold();
+        if(objectInReach!=null)
+            state = State.ObjectInReach;
+        else
+            state = State.NoObjectsInReach;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.gray;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
--- Assets/Scripts/Robot/functionality/Torpedo.cs
+++ Assets/Scripts/Robot/functionality/Torpedo.cs
@@ -1,31 +1,27 @@
 ﻿using UnityEngine;
 
-namespace Robot.Functionality
+public class Torpedo : MonoBehaviour
 {
-    public class Torpedo : MonoBehaviour
+    [SerializeField]
+    private float range = 10f;
+    [SerializeField]
+    private Transform fpsPosition=null;
+    [SerializeField]
+    private LayerMask mask=0;
+
+    public bool lastTorpedoHit { get; private set; } = false;
+
+    public void Shoot()
     {
-        [SerializeField]
-        private float range = 10f;
-        [SerializeField]
-        private Transform fpsPosition=null;
-        [SerializeField]
-        private LayerMask mask=0;
-
-        public bool lastTorpedoHit { get; private set; } = false;
-
-        public void Shoot()
+        RaycastHit hit;
+        if (Physics.Raycast(fpsPosition.transform.position, fpsPosition.transform.forward, out hit, range, mask))
         {
-            RaycastHit hit;
-            if (Physics.Raycast(fpsPosition.transform.position, fpsPosition.transform.forward, out hit, range, mask))
-            {
-                EventsLogger.Log("Torpedo hit an object marked as torpedo target.");
-                lastTorpedoHit = true;
-            } else
-            {
-                EventsLogger.Log("Torpedo missed.");
-                lastTorpedoHit = false;
-            }
+            EventsLogger.Log("Torpedo hit an object marked as torpedo target.");
+            lastTorpedoHit = true;
+        } else
+        {
+            EventsLogger.Log("Torpedo missed.");
+            lastTorpedoHit = false;
         }
     }
 }
-
