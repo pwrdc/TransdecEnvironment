dd6c4e13a35647d6380cbe0242f5f51e84aae15c
piotrszleg <szlegp@gmail.com>
2020-08-31 09:10:21 +0200

    Created depth reader post processing effect. It might be used for neural network training later but for now it'll be used to experiment with more precise water physics.

---
+++ Assets/Scripts/Effects/DepthReader.cs
@@ -0,0 +1,28 @@
+﻿
+// source: https://forum.unity.com/threads/legacy-blur-for-post-processing-stack-v2.488222/
+
+using System;
+
+namespace UnityEngine.Rendering.PostProcessing
+{
+    [Serializable]
+    [PostProcess(typeof(DepthReaderEffect), PostProcessEvent.AfterStack, "Custom/DepthReader")]
+    public sealed class DepthReader : PostProcessEffectSettings
+    {
+        public FloatParameter MaxDepth = new FloatParameter { value = 0.05f };
+        public ColorParameter ClearColor = new ColorParameter { value = Color.black };
+    }
+
+    public sealed class DepthReaderEffect : PostProcessEffectRenderer<DepthReader>
+    {
+
+        public override void Render(PostProcessRenderContext context)
+        {
+            PropertySheet sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/DepthReader"));
+            sheet.properties.Clear();
+            sheet.properties.SetFloat("_MaxDepth", settings.MaxDepth);
+            sheet.properties.SetColor("_ClearColor", settings.ClearColor);
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Effects/DepthReader.shader
@@ -0,0 +1,40 @@
+﻿Shader "Hidden/Custom/DepthReader"
+{
+	HLSLINCLUDE
+
+	#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+	#include "Packages/com.unity.postprocessing/PostProcessing/Shaders/Builtins/Fog.hlsl"
+
+	TEXTURE2D_SAMPLER2D(_CameraDepthTexture, sampler_CameraDepthTexture);
+	float _MaxDepth;
+	float4 _ClearColor;
+
+	float4 Frag(VaryingsDefault i) : SV_Target
+	{
+		float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, sampler_CameraDepthTexture, i.texcoordStereo);
+		if (depth == 0)
+			return _ClearColor;
+		depth = Linear01Depth(depth);
+		float value = 1 - depth / _MaxDepth;
+		value *= value;
+
+		return float4(value.xxx, 1);
+	}
+
+		ENDHLSL
+
+		SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+
+			Pass
+		{
+			HLSLPROGRAM
+
+				#pragma vertex VertDefault
+				#pragma fragment Frag
+
+			ENDHLSL
+		}
+	}
+}
