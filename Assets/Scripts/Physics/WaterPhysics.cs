dfad324f6385aa9b7d85811dbe1c7b0a32442ad2
piotrszleg <szlegp@gmail.com>
2020-09-23 02:29:31 +0200

    27 voxels in all axes instead of 9 planar. Bounds rotate with the rigidbody. Calculating highest and lowest point of each voxel. This allows better simulation of rotated, non-cubic bodies. Regions in WaterPhysics class.

--- Assets/Scripts/Physics/WaterPhysics.cs
+++ Assets/Scripts/Physics/WaterPhysics.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody))]
 public class WaterPhysics : MonoBehaviour
 {
+    #region inspector
     public enum BuoyancyForceMode
     {
         FullySimulated,
@@ -19,9 +20,8 @@ public class WaterPhysics : MonoBehaviour
     public BuoyancyForceMode buoyancyForceMode;
     bool FullySimulated => buoyancyForceMode == BuoyancyForceMode.FullySimulated;
     bool FluctuationsOnly => buoyancyForceMode == BuoyancyForceMode.FluctuationsOnly;
-
-    new Collider collider;
-    public Bounds bounds=>collider!=null ? collider.bounds : new Bounds();
+    
+    public Bounds bounds;
     [ShowIf(EConditionOperator.Or, "FullySimulated", "deduceMassFromVolume")]
     [Range(0f, 1f), Tooltip("How much of the volume inside of the bounds is filled by the material.")]
     public float boundsFillLevel = 0.25f;
@@ -57,7 +57,30 @@ public class WaterPhysics : MonoBehaviour
     [ShowNativeProperty]
     float DeducedMass => Volume * ActualDensity;
 
+    #endregion
+
+    #region initialization
+
     Rigidbody body;
+
+    void EnsureBounds()
+    {
+        if (bounds == null)
+        {
+            Collider collider = GetComponentInChildren<Collider>();
+            bounds = collider != null ? collider.bounds : new Bounds();
+            bounds.center -= transform.position;
+        }
+    }
+
+    void Start()
+    {
+        EnsureBounds();
+        body = GetComponent<Rigidbody>();
+        body.drag = 1f;
+        body.angularDrag = 1f;
+        SetGravity();
+    }
 
     bool IsUnderWater(Vector3 position) => Environment.Environment.Instance.IsUnderWater(position.y);
 
@@ -68,27 +91,65 @@ public class WaterPhysics : MonoBehaviour
         else
             body.useGravity = true;
     }
-    
-    void Start()
+
+    #endregion
+
+    #region buoyancy
+
+    struct VerticalBounds
     {
-        collider = GetComponentInChildren<Collider>();
-        body = GetComponent<Rigidbody>();
-        body.drag = 1f;
-        body.angularDrag = 1f;
-        SetGravity();
+        public float lower;
+        public float higher;
     }
 
-    float VolumeInWater(Vector3 point)
+    // here we calculate lowest and highest point for the central voxel 
+    // because all voxels have the same size and same rotation 
+    // we can just add these values to their centers to get their lowest and highest points 
+    VerticalBounds GetVerticalBounds()
     {
-        Vector3 lower = point + Vector3.up * bounds.extents.y;
-        Vector3 upper = point - Vector3.up * bounds.extents.y;
+        Vector3[] cubeCorners = new Vector3[]
+        {
+            new Vector3(1,   1,  1),
+            new Vector3(-1,  1,  1),
+            new Vector3(1,   1, -1),
+            new Vector3(-1,  1, -1),
+            new Vector3(1,  -1,  1),
+            new Vector3(-1, -1,  1),
+            new Vector3(1,  -1, -1),
+            new Vector3(-1, -1, -1),
+        };
+        Vector3 cubeCornerToVoxelCorner(Vector3 corner)
+        {
+            // transform cube corner into bounds corner 
+            corner = Utils.MultiplyVectorsFields(corner, bounds.extents);
+            // there are three voxels in each direction, we use central one as a reference
+            corner /= 3;
+            // rotate the corner (this is the key step)
+            corner = transform.rotation * corner;
+
+            return corner;
+        }
+        IEnumerable<Vector3> centralVoxelCorners = cubeCorners.Select(cubeCornerToVoxelCorner);
+
+        return new VerticalBounds
+        {
+            higher = centralVoxelCorners.Max(corner => corner.y),
+            lower = centralVoxelCorners.Min(corner => corner.y)
+        };
+    }
+
+    float VolumeInWater(Vector3 point, VerticalBounds verticalBounds)
+    {
+        Vector3 lower = point + Vector3.up * verticalBounds.lower;
+        Vector3 upper = point - Vector3.up * verticalBounds.higher;
         return 1f-Mathf.InverseLerp(lower.y, upper.y, Environment.Environment.Instance.GetWaterY());
     }
 
-    float CalculateBuoyancyForce(Vector3 position, float volumePart)
+    float CalculateBuoyancyForce(Vector3 position, float volumePart, VerticalBounds verticalBounds)
     {
         if (!IsUnderWater(position))
-        return 0f;
+            return 0f;
+
         float force;
         switch (buoyancyForceMode)
         {
@@ -103,20 +164,31 @@ public class WaterPhysics : MonoBehaviour
             default:
                 throw new InvalidEnumValueException(buoyancyForceMode);
         }
-        return force * VolumeInWater(position);
+        return force * VolumeInWater(position, verticalBounds);
     }
 
-    void AddBuoyancyForce(Vector3 position, float volumePart)
+    void AddBuoyancyForce(Vector3 position, float volumePart, VerticalBounds verticalBounds)
     {
         if (IsUnderWater(position))// this additional check ensures that we won't add zero force
         {
-            body.AddForceAtPosition(Vector3.up * CalculateBuoyancyForce(position, volumePart), position);
+            body.AddForceAtPosition(Vector3.up * CalculateBuoyancyForce(position, volumePart, verticalBounds), position);
         }
     }
 
+    // these points are analogous to rubic cube's parts' centers
     IEnumerable<Vector3> ForcePositions => new Vector3[]
     {
-        Vector3.zero,
+        new Vector3(0, -1, 0),
+        new Vector3(1, -1, 1),
+        new Vector3(-1, -1, 1),
+        new Vector3(1, -1, -1),
+        new Vector3(-1, -1, -1),
+        new Vector3(1, -1, 0),
+        new Vector3(0, -1, 1),
+        new Vector3(-1, -1, 0),
+        new Vector3(0, -1, -1),
+
+        new Vector3(0, 0, 0),
         new Vector3(1, 0, 1),
         new Vector3(-1, 0, 1),
         new Vector3(1, 0, -1),
@@ -124,17 +196,30 @@ public class WaterPhysics : MonoBehaviour
         new Vector3(1, 0, 0),
         new Vector3(0, 0, 1),
         new Vector3(-1, 0, 0),
-        new Vector3(0, 0, -1)
-    }.Select(offset=>Utils.MultiplyVectorsFields(offset, bounds.extents) + bounds.center);
-    float VolumePerOffset=> Volume / 9;
+        new Vector3(0, 0, -1),
+
+        new Vector3(0, 1, 0),
+        new Vector3(1, 1, 1),
+        new Vector3(-1, 1, 1),
+        new Vector3(1, 1, -1),
+        new Vector3(-1, 1, -1),
+        new Vector3(1, 1, 0),
+        new Vector3(0, 1, 1),
+        new Vector3(-1, 1, 0),
+        new Vector3(0, 1, -1)
+    }.Select(offset=>transform.TransformPoint(Utils.MultiplyVectorsFields(offset, bounds.extents*(2f/3f)) + bounds.center));
+    float VolumePerOffset=> Volume / 27;
 
     void UpdateBuoyancyForces()
     {
+        VerticalBounds verticalBounds = GetVerticalBounds();
         foreach(var offset in ForcePositions)
         {
-            AddBuoyancyForce(offset, VolumePerOffset);
+            AddBuoyancyForce(offset, VolumePerOffset, verticalBounds);
         }
     }
+
+    #endregion
 
     void AddCurrent()
     {
@@ -158,16 +243,31 @@ public class WaterPhysics : MonoBehaviour
 
     private void OnDrawGizmosSelected()
     {
-        if (collider == null)
-        {
-            collider = GetComponentInChildren<Collider>();
-        }
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireCube(bounds.center, bounds.size);
+        EnsureBounds();
 
-        foreach (var position in ForcePositions)
+        Gizmos.color = Color.cyan;
+        
+        // rotate and translate the matrix to draw the bounds
+        Matrix4x4 saved=Gizmos.matrix;
+        Gizmos.matrix *= Matrix4x4.Translate(transform.position + bounds.center);
+        Gizmos.matrix *= Matrix4x4.Rotate(transform.rotation);
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+        // restore the matrix
+        Gizmos.matrix = saved;
+
+        if (buoyancyForceMode != BuoyancyForceMode.Disabled)
         {
-            Gizmos.DrawRay(position, Vector3.up * CalculateBuoyancyForce(position, VolumePerOffset));
+            VerticalBounds verticalBounds = GetVerticalBounds();
+            foreach (var position in ForcePositions)
+            {
+                // draw the buoyancy force
+                Gizmos.DrawRay(position, Vector3.up * CalculateBuoyancyForce(position, VolumePerOffset, verticalBounds));
+                // draw lower and upper bounds of the voxel
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawLine(position + Vector3.up * verticalBounds.lower, position + Vector3.up * verticalBounds.higher);
+                // restore the color
+                Gizmos.color = Color.cyan;
+            }
         }
     }
 }
